75f8156a59871f8ac7d52886927b052d810eb040
HurryPeng <i@hurrypeng.cc>
2019-07-11 19:54:19 +0800

    Integrated FTPTest code into IntelliEco

---
+++ Android/IntelliEco/app/src/main/java/cc/hurrypeng/intellieco/DataPack.java
@@ -0,0 +1,12 @@
+package cc.hurrypeng.intellieco;
+
+class DataPack {
+    static class Login {
+        String username;
+        String password;
+        String request;
+    }
+    static class LoginResponse {
+        String authority;
+    }
+}
